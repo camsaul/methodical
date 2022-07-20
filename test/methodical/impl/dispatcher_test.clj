8dc7d02702f557c4d0cf03ed0a69cf6e46765f06
Cam Saul <github@camsaul.com>
2022-07-20 16:17:51 -0700

    Add test for #31

--- test/methodical/impl/dispatcher_test.clj
+++ test/methodical/impl/dispatcher_test.clj
@@ -28,3 +28,40 @@
                                                           {:bird k})))]
             (t/is (= {:parrot :parroty}
                      (multifn :parroty)))))))))
+
+(t/deftest list-composite-dispatch-values-test
+  (t/testing "Dispatch values should match if they are EQUAL (`=`) if we mix and match vectors/lists (#31)"
+    ;; this means `[:bird String]` and `(:bird String)` should be treated as the same. However,
+    ;;
+    ;;    (isa? [:bird String] '(:bird Object))
+    ;;
+    ;; or vice versa does not hold true. Only vectors should do recursive `isa?` matching -- this is what vanilla
+    ;; multimethods do.
+    (doseq [dispatch-fn                     [(fn [x y]
+                                               (list (keyword x) (class y)))
+                                             (fn [x y]
+                                               [(keyword x) (class y)])]
+            method-dispatch-value           [[:bird String]
+                                             (list :bird String)]
+            [dispatcher-message dispatcher] {"everything dispatcher"
+                                             (m/everything-dispatcher)
+
+                                             "standard dispatcher"
+                                             (m/standard-dispatcher dispatch-fn)
+
+                                             "multi-default-dispatcher"
+                                             (m/multi-default-dispatcher dispatch-fn)}]
+      (t/testing (str \newline dispatcher-message)
+        (t/testing (format "\ndispatch value => %s" (pr-str (dispatch-fn :bird "chirp")))
+          (t/testing (str \newline (pr-str (list 'add-primary-method method-dispatch-value '...)))
+            (let [multifn (-> (m/multifn
+                               (m/standard-multifn-impl
+                                (m/thread-last-method-combination)
+                                dispatcher
+                                (m/standard-method-table)))
+                              (m/add-primary-method method-dispatch-value (fn String-method [_next-method x y]
+                                                                            {:bird x, String y}))
+                              (m/add-primary-method [:bird Object] (fn Object-method [_next-method x y]
+                                                                     {:bird x, Object y})))]
+              (t/is (= {:bird :bird, String "chirp"}
+                       (multifn :bird "chirp"))))))))))
